00d87dda830effa1a5fc7624d74112bc410cfcd7
Zaphiro3D <uujito.17@gmail.com>
2024-10-29 00:09:02 -0300

    asd

--- modelos/modelo.cargos.php
+++ modelos/modelo.cargos.php
@@ -7,26 +7,36 @@ class ModeloCargos{
     static public function mdlMostrarCargos()
     {
         try {
-            $cargos = Conexion::conectar()->prepare("SELECT c.id_Cargo, nc.nombreCargo, c.hsCatedra,g.grado, d.division, t.turno, 
-                c.apellidoDocente, c.nombreDocente, c.dniDocente, tipo.tipo, i.numero, i.nombre as nombreInsti, i.cue 
-                FROM 
-                `cargos` as c 
-                INNER JOIN `plazas` as p
-                ON p.id_Cargo = c.id_Cargo
-                INNER JOIN `instituciones` as i
-                ON p.id_Institucion = i.id_Institucion
-                LEFT JOIN `grados` as g
-                ON g.id_Grado = c.id_Grado 
-                LEFT JOIN `divisiones` as d 
-                ON d.id_Division = c.id_Division
-                LEFT JOIN `turnos` as t
-                ON t.id_Turno = c.id_Turno
-                LEFT JOIN `nombres_cargos` as nc 
-                ON nc.id_NombreCargo = c.id_NombreCargo
-                LEFT JOIN `tipo_institucion` as tipo 
-                ON tipo.id_Tipo = i.id_Tipo
-                Where c.id_Cargo = p.id_Cargo and c.eliminado = 0;
-                ");
+            $cargos = Conexion::conectar()->prepare(
+            "SELECT 
+                c.id_Cargo, 
+                nc.nombreCargo, 
+                c.hsCatedra,
+                g.grado, 
+                d.division, 
+                t.turno, 
+                CONCAT(c.apellidoDocente, ', ' ,c.nombreDocente,' (', c.dniDocente, ') ') as docente,
+                tipo.tipo, 
+                GROUP_CONCAT(
+                    CONCAT(
+                        i.numero, ' - ', i.nombre, ' (CUE: ', i.cue, ')'
+                    ) ORDER BY p.sede DESC
+                ) AS instituciones
+            FROM 
+                `cargos` AS c 
+                INNER JOIN `plazas` AS p ON p.id_Cargo = c.id_Cargo
+                INNER JOIN `instituciones` AS i ON p.id_Institucion = i.id_Institucion
+                LEFT JOIN `grados` AS g ON g.id_Grado = c.id_Grado 
+                LEFT JOIN `divisiones` AS d ON d.id_Division = c.id_Division
+                LEFT JOIN `turnos` AS t ON t.id_Turno = c.id_Turno
+                LEFT JOIN `nombres_cargos` AS nc ON nc.id_NombreCargo = c.id_NombreCargo
+                LEFT JOIN `tipo_institucion` AS tipo ON tipo.id_Tipo = i.id_Tipo
+            WHERE 
+                c.eliminado = 0
+            GROUP BY 
+                c.id_Cargo;"
+            );
+
             $cargos->execute();
             return $cargos->fetchAll(PDO::FETCH_ASSOC);
 
--- vistas/modulos/cargos.php
+++ vistas/modulos/cargos.php
@@ -6,7 +6,7 @@
     </div>
     <div class="py-2 d-flex align-items-sm-center flex-sm-row flex-column">
         <div class="d-flex flex-wrap gap-2">    
-            <a href="nuevo_agente" class="btn btn-primary"><i class="fas fa-plus" ></i > &nbsp; Nuevo Cargo</a>
+            <a href="nuevo_cargo" class="btn btn-primary"><i class="fas fa-plus" ></i > &nbsp; Nuevo Cargo</a>
         </div> 
     </div>
     
@@ -17,13 +17,17 @@
                     <table id="tablaES" class="table table-striped dt-responsive nowrap w-100">
                         <thead>
                             <tr>
-                                <th>Establecimiento</th>    
+                                <!-- <th>Establecimiento</th>     -->
                                 <th>Cargo</th>    
                                 <th>Hs. Cátedra</th>
                                 <th>Grado</th>
                                 <th>División</th>
                                 <th>Turno</th>
-                                <th>Docente</th>                                 
+                                <th>Docente</th>
+                                <th>Int. Sede</th>
+                                <th>Institución 2</th>
+                                <th>Institución 3</th>
+                                <th>Institución 4</th>                                 
                                 <th>Acciones</th>
                             </tr>
                         </thead>
@@ -31,17 +35,33 @@
                             <?php
 
                                 $cargos = ControladorCargos::ctrMostrarCargos();
+                                
                                 foreach ($cargos as $key => $value) {                         
                             ?>
                             <tr style = "background-color:#000888">
-                                <td> <?php echo $value["tipo"] . " N°" .  $value["numero"] . ' "' . $value["nombreInsti"] . '" CUE:' . $value["cue"] ?></td>    
+                                <!-- <td> <?php echo $value["tipo"] . " N°" .  $value["numero"] . ' "' . $value["nombreInsti"] . '" CUE:' . $value["cue"] ?></td>     -->
                                 <td> <?php echo $value["nombreCargo"] ?></td>
                                 <td> <?php echo $value["hsCatedra"] ?></td>
                                 <td> <?php echo $value["grado"] ?></td>
                                 <td> <?php echo $value["division"] ?></td>
                                 <td> <?php echo $value["turno"] ?></td>
-                                <td> <?php echo $value["apellidoDocente"] . ", " . $value["nombreDocente"] . " (" . $value["dniDocente"] .")" ?></td>
+                                <td> <?php echo $value["docente"] ?></td>
+                                
+
+                                <?php
+                                // Procesa las instituciones y las divide en columnas
+                                $instituciones = explode(',', $value['instituciones']);
+                                for ($i = 0; $i < 4; $i++) {
+                                    if (isset($instituciones[$i])) {
+                                        echo "<td>" . $instituciones[$i] . "</td>";
+                                    } else {
+                                        echo "<td></td>";  // Celda vacía si no hay más instituciones
+                                    }
+                                }
+                                ?>
+
                                 <td><a href="editar_cargo" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i></a> <a href="eliminar_cargo" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a></td>
+                                
                             </tr>
 
                             <?php } ?>
